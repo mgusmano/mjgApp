7e358913ec8df609b51f70fb19df85fac292ca4d
Marc Gusmano <mgusmano@yahoo.com>
2013-11-11 16:13:56 -0600

    normal

--- app/view/PastPhone.js
+++ app/view/PastPhone.js
@@ -6,12 +6,12 @@ Ext.define('mjgApp.view.PastPhone', {
         direction: 'horizontal',
         items: [
             { xtype: 'container', padding: '15 15 15 15', contentEl: 'a', style: { backgroundColor: '#FFFFFF' } },
-            { xtype: 'container', padding: '5 5 5 5', contentEl: 'b' },
-            { xtype: 'container', padding: '5 5 5 5', contentEl: 'c' },
-            { xtype: 'container', padding: '5 5 5 5', contentEl: 'd' },
-            { xtype: 'container', padding: '5 5 5 5', contentEl: 'e' },
-            { xtype: 'container', padding: '5 5 5 5', contentEl: 'f' },
-            { xtype: 'container', padding: '5 5 5 5', contentEl: 'g' }
+            { xtype: 'container', padding: '15 15 15 15', contentEl: 'b' },
+            { xtype: 'container', padding: '15 15 15 15', contentEl: 'c', style: { backgroundColor: '#FFFFFF' } },
+            { xtype: 'container', padding: '15 15 15 15', contentEl: 'd' },
+            { xtype: 'container', padding: '15 15 15 15', contentEl: 'e', style: { backgroundColor: '#FFFFFF' } },
+            { xtype: 'container', padding: '15 15 15 15', contentEl: 'f' },
+            { xtype: 'container', padding: '15 15 15 15', contentEl: 'g', style: { backgroundColor: '#FFFFFF' } }
         ]
     }
 });
