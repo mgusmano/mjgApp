d404ec54175b2284ba2b8af52d86e5bef29a31c9
Marc Gusmano <mgusmano@yahoo.com>
2013-11-12 12:16:28 -0600

    normal

--- app/view/AllTablet.js
+++ app/view/AllTablet.js
@@ -5,19 +5,15 @@ Ext.define('mjgApp.view.AllTablet', {
     isLoaded: false,
     listeners: {
         painted: function (element, eOpts) {
-            debugger;
             if (this.isLoaded === false) {
                 this.isLoaded = true;
                 var $container = $('#flip'),
                     $pages = $container.children().hide();
-
-                debugger;
                 Modernizr.load({
                     test: Modernizr.csstransforms3d && Modernizr.csstransitions,
                     yep: ['js/jquery.tmpl.min.js', 'js/jquery.history.js', 'js/core.string.js', 'js/jquery.touchSwipe-1.2.5.js', 'js/jquery.flips.js'],
                     nope: 'css/fallback.css',
                     callback: function (url, result, key) {
-                        debugger;
                         if (url === 'css/fallback.css') {
                             $pages.show();
                         }
--- build/testing/mjgApp/app.js
+++ build/testing/mjgApp/app.js
@@ -67836,7 +67836,6 @@ Ext.define('mjgApp.view.AllTablet', {
                 this.isLoaded = true;
                 var $container = $('#flip'),
                     $pages = $container.children().hide();
-
                 Modernizr.load({
                     test: Modernizr.csstransforms3d && Modernizr.csstransitions,
                     yep: ['js/jquery.tmpl.min.js', 'js/jquery.history.js', 'js/core.string.js', 'js/jquery.touchSwipe-1.2.5.js', 'js/jquery.flips.js'],
